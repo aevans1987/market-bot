6063dc7e6b8fd7f63ff2c765b05b92369e58971f
Andrew Evans <ade1987@pm.me>
2024-10-28 18:11:43 -0400

    When joining a guild, create a DB for that guild to ensure each guild has its own info

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,7 +6,7 @@ module.exports = {
 	async execute(guild) {
         const query = `CREATE TABLE public."${guild.id}"() INHERITS (public.template);`
         console.log(`Bot has been added to a new server: ${guild.id}/${guild.name}`)
-		const result = db_query(query)
+		const result = db_query(query, "create")
         console.log(`Action ${result}`)
 	},
 };
