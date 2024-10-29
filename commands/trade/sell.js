4818f5d02e44af45143e46f64940e71b5025ad74
Andrew Evans <ade1987@pm.me>
2024-10-28 20:15:46 -0400

    updating sell info

--- commands/trade/sell.js
+++ commands/trade/sell.js
@@ -26,6 +26,6 @@ module.exports = {
         const query = `INSERT INTO public."${interaction.guild.id}" (seller, product, price, availability) VALUES (${interaction.user.id}, '${interaction.options.getString('product').toUpperCase()}', ${interaction.options.getNumber('price')}, '${interaction.options.getString('availability') ?? 'N/A'}')`;
         const result = await db_query(query, 'create');
         console.log(result)
-        await interaction.reply({content: `Selling ${interaction.options.getString('product')} at ${interaction.options.getNumber('price')} with no listed availability ${result}.`, ephemeral: true })
+        await interaction.reply({content: `Selling ${interaction.options.getString('product')} at ${interaction.options.getNumber('price')} with ${interaction.options.getString('availability') ?? 'no listed' } availability ${result}.`, ephemeral: true })
     },
 };
