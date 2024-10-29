8315614cca9d5b0f66c2883ac4ea7a35e5e83b88
Andrew Evans <ade1987@pm.me>
2024-10-28 21:41:24 -0400

    help file

---
+++ commands/utility/help.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Replies with Bot Command Information!'),
+	async execute(interaction) {
+        const array = [
+            {
+                name: "/help",
+                value: "Displays this help command!"
+            },
+            {
+                name: "/sell",
+                value: "Allows you to list products for sale."
+            },
+            {
+                name: "/buy",
+                value: "Allows you to search orders that have been registered for a product."
+            },
+            {
+                name: "/myorders",
+                value: "Shows you your registered sell orders."
+            },
+            {
+                name: "/deleteorder",
+                value: "Allows you to delete an existing order with the order id."
+            },
+            {
+                name: "/adminorders",
+                value: "Allows an Admin to view all orders posted by a user."
+            },
+            {
+                name: "/admindelete",
+                value: "Allows an Admin to delete an order posted by a user."
+            }
+        ]
+	},
+};
